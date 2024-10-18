dd3bf60ea7017098f92951a04dd44a50579e1df0
Bikram Parajuli <bikramparajuli0000@gmail.com>
2024-10-18 21:41:54 +0545

    minor fixes

--- apps/vite/src/Body.tsx
+++ apps/vite/src/Body.tsx
@@ -1,37 +1,34 @@
-import { useContext } from "react";
-import { Route, Routes } from "react-router-dom";
-import AnnouncementAlert from "./components/AnnouncementAlert.tsx";
-import { DarkModeContext } from "./components/DarkModeProvider.tsx";
-import Footer from "./components/Footer.tsx";
-import Navbar from "./components/Navbar.tsx";
-import About from "./pages/About.tsx";
-import DateConverter from "./pages/DateConverter.tsx";
-import GoogleApiDisclosure from "./pages/GoogleApiDisclosure.tsx";
-import Home from "./pages/Home.tsx";
-import PrivacyPolicy from "./pages/PrivacyPolicy.tsx";
+import { useContext } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import AnnouncementAlert from './components/AnnouncementAlert'
+import { DarkModeContext } from './components/DarkModeProvider'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import About from './pages/About'
+import DateConverter from './pages/DateConverter'
+import GoogleApiDisclosure from './pages/GoogleApiDisclosure'
+import Home from './pages/Home'
+import PrivacyPolicy from './pages/PrivacyPolicy'
 
 const Body = () => {
-	const { darkMode } = useContext(DarkModeContext);
+  const { darkMode } = useContext(DarkModeContext)
 
-	return (
-		<div className={(darkMode ? "dark" : "") + " flex min-h-screen flex-col"}>
-			<AnnouncementAlert />
-			<Navbar />
-			<div className="flex-grow">
-				<Routes>
-					<Route path="/:pageType?/:BSYear?/:BSMonth?" element={<Home />} />
-					<Route path="/privacy" element={<PrivacyPolicy />} />
-					<Route path="/converter" element={<DateConverter />} />
-					<Route path="/about" element={<About />} />
-					<Route
-						path="/google-api-disclosure"
-						element={<GoogleApiDisclosure />}
-					/>
-				</Routes>
-			</div>
-			<Footer />
-		</div>
-	);
-};
+  return (
+    <div className={(darkMode ? 'dark' : '') + ' flex min-h-screen flex-col'}>
+      <AnnouncementAlert />
+      <Navbar />
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/:pageType?/:BSYear?/:BSMonth?" element={<Home />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/converter" element={<DateConverter />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/google-api-disclosure" element={<GoogleApiDisclosure />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  )
+}
 
-export default Body;
+export default Body
--- apps/vite/src/main.tsx
+++ apps/vite/src/main.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { initSW } from "./lib/sw-helper.js";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { initSW } from './lib/sw-helper.js'
 
-import App from "./App.tsx";
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
-if (import.meta.env.MODE !== "development") {
-  initSW();
+if (import.meta.env.MODE !== 'development') {
+  initSW()
 }
