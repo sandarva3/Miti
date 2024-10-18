60715532dd5a310fea136cade130213a734282d0
Bikram Parajuli <bikramparajuli0000@gmail.com>
2024-10-18 17:57:46 +0545

    feat: :sparkles: Migrated to v51 for expo

--- apps/plasmo/popup.tsx
+++ apps/plasmo/popup.tsx
@@ -9,14 +9,14 @@ function IndexPopup() {
         padding: 16
       }}>
       <h2>
-        Welcome to my{" "}
-        <a href="https://www.plasmo.com" target="_blank" rel="noreferrer">
+        Welcome to your{" "}
+        <a href="https://www.plasmo.com" target="_blank">
           Plasmo
         </a>{" "}
         Extension!
       </h2>
       <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank" rel="noreferrer">
+      <a href="https://docs.plasmo.com" target="_blank">
         View Docs
       </a>
     </div>
