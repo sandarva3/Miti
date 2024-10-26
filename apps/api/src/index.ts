b5ca763b2593419e1fa1986f51c6b44bc8a3dffd
Bikram Parajuli <bikramparajuli0000@gmail.com>
2024-10-26 12:24:26 +0545

    minor changes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,7 +2,6 @@ import { Hono } from "hono";
 import { env } from "hono/adapter";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
-import { generateId } from "lucia";
 
 import { authMiddleware } from "./auth/auth.middleware";
 import { initializeLucia } from "./auth/lucia-auth";
@@ -12,17 +11,13 @@ import { GoogleCalendarController } from "./controller/calendar/google-calendar.
 import { UserController } from "./controller/user/user.controller";
 import { initalizeDB } from "./database/db";
 import { installTable } from "./database/installs";
-import { oauthAccountTable } from "./database/oauth.accounts";
-import { userTable } from "./database/users";
-
-// import json from "./users.json";
 
 const app = new Hono<AppContext>();
 
 app
   .use(logger())
   .use((c, next) => {
-    const handler = cors({ origin: env(c).WEB_DOMAIN, credentials: true });
+    const handler = cors({ origin: [env(c).WEB_DOMAIN, env(c).API_DOMAIN], credentials: true });
     return handler(c, next);
   })
   .use((c, next) => {
@@ -51,32 +46,6 @@ app
       });
     return c.json({ status: "ok" });
   })
-  // .get("/seed", async (c) => {
-  //   await Promise.all(
-  //     json.map(async (item) => {
-  //       const userId = await c
-  //         .get("db")
-  //         .insert(userTable)
-  //         .values({
-  //           email: item._json.email,
-  //           username: item._json.name,
-  //           emailVerified: item._json.email_verified ? 1 : 0,
-  //           refreshToken: item.refreshToken,
-  //           id: generateId(15),
-  //           profilePictureUrl: item._json.picture,
-  //         })
-  //         .returning()
-  //         .then((res) => res[0]?.id ?? "");
-  //       await c.get("db").insert(oauthAccountTable).values({
-  //         userId,
-  //         provider: "google",
-  //         providerUserId: item._json.sub,
-  //       });
-  //     })
-  //   );
-
-  //   return c.json(json.map((item) => item._json.name));
-  // })
   .get("/health", (c) => {
     return c.json({ status: "ok" });
   })
