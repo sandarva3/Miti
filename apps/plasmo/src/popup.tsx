0150a8d6610c3c553ec7b9014b83e34afe2fda57
Bikram Parajuli <bikramparajuli0000@gmail.com>
2024-11-10 10:38:21 +0545

    feat: :sparkles: Changed Icon based on date

--- apps/plasmo/src/popup.tsx
+++ apps/plasmo/src/popup.tsx
@@ -2,7 +2,17 @@ import Calendar from "~components/Calendar/Calendar"
 
 import "~style.css"
 
+import NepaliDate from "nepali-datetime"
+import { useEffect } from "react"
+
+import { getIconSrcByDay } from "~utils/icons"
+
 function IndexPopup() {
+  useEffect(() => {
+    const today = new NepaliDate().getDate()
+    console.log(chrome)
+    chrome?.action?.setIcon({ path: getIconSrcByDay(today) })
+  }, [])
   return (
     <div className="plasmo-h-full plasmo-w-[30rem] plasmo-bg-[#1F2937] plasmo-pt-[0.15rem] plasmo-overflow-hidden">
       <Calendar />
